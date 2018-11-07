96119a96b9b4ec83d53e58ba06f33e21742cdd2c
roger-mahler <roger.mahler@hotmail.com>
2018-11-07 10:31:34 +0100

    workshop revisions

--- daedalus_notebooks/common/plot_utility.py
+++ daedalus_notebooks/common/plot_utility.py
@@ -20,11 +20,11 @@ if 'extend' not in globals():
     
 class WordcloudUtility:
     
-    def plot_wordcloud(df_data, token='token', weight='weight', **args):
+    def plot_wordcloud(df_data, token='token', weight='weight', figsize=(12, 12), dpi=100, **args):
         token_weights = dict({ tuple(x) for x in df_data[[token, weight]].values })
         image = wordcloud.WordCloud(**args,)
         image.fit_words(token_weights)
-        plt.figure(figsize=(12, 12)) #, dpi=100)
+        plt.figure(figsize=figsize, dpi=dpi)
         plt.imshow(image, interpolation='bilinear')
         plt.axis("off")
         # plt.set_facecolor('w')
